28b6416606d6671600ef04b2d5d6d4489c2f93b2
trieungochai <trieungochai.dev@gmail.com>
2021-09-30 21:56:52 +0700

    HTTP - HTML File

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+// Get all files
+const homePage = readFileSync("./index.html");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Home page</h1>");
+    res.write(homePage);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
