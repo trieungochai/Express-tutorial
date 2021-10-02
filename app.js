735f4f5aef0a98ac0dc03765c62d5a28493bb2bb
trieungochai <trieungochai.dev@gmail.com>
2021-10-02 15:47:51 +0700

    Params, Query String - Setup

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+
+  res.json(newProducts);
 });
 
 app.listen(3000, () => {
