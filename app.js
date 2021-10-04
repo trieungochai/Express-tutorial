8aa72e98899e8049d4c74225294be76eb52e55c9
trieungochai <trieungochai.dev@gmail.com>
2021-10-05 03:21:47 +0700

    Method - POST (Form Example)

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
+// static assets
+app.use(express.static("./methods-public"));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  } else {
+    return res.status(401).send("Please Provide Credentials");
+  }
 });
 
 app.listen(3000, () => {
