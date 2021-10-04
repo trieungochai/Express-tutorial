1fd825b9f3595caefb12308bf69db4165b6889c3
trieungochai <trieungochai.dev@gmail.com>
2021-10-05 01:56:36 +0700

    APP.USE

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require("express");
+const logger = require("./logger");
 
 const app = express();
 
 // req => middleware => res
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
+app.use("/api", logger);
 
 app.get("/", (req, res) => {
   res.send("Home");
@@ -17,6 +12,14 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(3000, () => {
